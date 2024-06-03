e903be08b669a2ed49234d8ee3a470e7399f137f
Akshay Sharma <akkidpic39@gmail.com>
2024-06-03 16:46:49 +0530

    chapter-1 about basic react library

--- App.js
+++ App.js
@@ -1,5 +1,39 @@
-const heading = React.createElement("h1", {id:"heading"}, "hello world from react");
+/* 
+<div id ="parent">
+    <div id="child">
+        <h1>i'm h1 tag</h1>
+        <h2>i'm h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>i'm h1 tag</h1>
+        <h2>i'm h2 tag</h2>
+    </div>
+
+</div>
+
+
+ReactElement(oject) => HTML(browser understands)
+
+
+*/
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", {id:"child"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+    React.createElement("div", {id:"child2"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+    
+  ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+// this is the main reason we dont use this type of format instead of JSX.
