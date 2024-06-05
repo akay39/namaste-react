216c30ea9caa978f7f6d4418c4f1c7c470d7bf02
Akshay Sharma <akkidpic39@gmail.com>
2024-06-05 19:34:04 +0530

    laying the foundation with jsx and functional components.

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", {id:"child"},[
-        React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h2 tag"),
-    ]),
-    React.createElement("div", {id:"child2"},[
-        React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h2 tag"),
-    ]),
-    
-  ]
-);
-console.log(parent);
+//React Element: is an object
+//when rendered to the dom then it becomes html element.
 
+//JSX - not HTML in JS, HTML/XML likes syntax
+// parcel manages babel to transpiles JSX code and return the code that js engine understands.
+// JSX with the help of babel converte to react.createElement after that js object and then finally html.
+// we wont be using react.createElement from now.
+
+//react element:line-15
+const jsxHeading = <h1>Namaste React using React jsx.</h1>;
+
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//root.render(jsxHeading);
+
+// React component:
+//1. class based component- old way
+//2. functional based component - new way of writing code
+// its a nomral js function which returns some JSX code/react elements.
+
+const elem = <span>element react.</span>;
+
+const title = (
+  <h1 className="head">
+     namaste react using JSX
+  </h1>
+);
+
+const Headingcomponent = () => {
+  return (
+    <div id="container">
+      {title}
+      <h1>namaste react functional component</h1>
+    </div>
+  );
+};
+
+root.render(<Headingcomponent />);
+
+// we can write any js inside {} of JSX.
+//injecting reactElement inside the component by just using {name of the element inside curly braces.}
+// we can inject any react element, component in element and component. 
