53378281eaa50b9acc5153818a599810979c6c89
Akshay Sharma <akkidpic39@gmail.com>
2024-06-09 19:39:33 +0530

    new change to useState with the default value updatation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,73 +1,12 @@
 import ResturantCard from "./ResturantCard.";
 import { useState } from "react";
-//import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 
 const Body = () => {
 
     // local state variable - super powerful variable
 
-    const [listOfResturants, setListOfResturant] = useState([  {info: {
-        id: "36658",
-        name: "Dominos",
-        cloudinaryImageId: "5116a385bac0548e06c33c08350fbf11",
-        locality: "Panjra Pole",
-        areaName: "Ambawadi",
-        costForTwo: "₹200 for two",
-        deliveryTime: 30,
-        cuisines: [
-          "Waffle",
-          "Desserts",
-          "Ice Cream",
-          "Beverages"
-        ],
-        avgRating: 4.5,
-        veg: true,
-        parentId: "2233",
-        avgRatingString: "4.5",
-        }
-    },
-      {
-        info: {
-          id: "36657",
-          name: "The Belgian Waffle Co.",
-          cloudinaryImageId: "5116a385bac0548e06c33c08350fbf11",
-          locality: "Panjra Pole",
-          areaName: "Ambawadi",
-          costForTwo: "₹200 for two",
-          deliveryTime: 30,
-          cuisines: [
-            "Waffle",
-            "Desserts",
-            "Ice Cream",
-            "Beverages"
-          ],
-          avgRating: 3.9,
-          veg: true,
-          parentId: "2233",
-          avgRatingString: "4.5",
-          
-      }
-    },
-      {
-        info: {
-          id: "52630",
-          name: "McDonald's",
-          cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/4a8bc67d-8fe4-44db-be5d-1477086cc52b_52630.JPG",
-          locality: "Ellis Bridge",
-          areaName: "Ashram Road",
-          costForTwo: "₹400 for two",
-          deliveryTime: 21,
-          cuisines: [
-            "Burgers",
-            "Beverages",
-            "Cafe",
-            "Desserts"
-          ],
-          avgRating: 4.4,
-          parentId: "630",
-          avgRatingString: "4.4",
-      }
-    },]);
+    const [listOfResturants, setListOfResturant] = useState( resList);
 
 // Normal JS Variable
 // let listOfResturants2 = [
